e837395440f38d2c556f54204e527c24b279f1e2
ISH k pro <princeishimwe754@gmail.com>
2025-10-09 19:32:45 +0200

    Implemented neon database connection.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv";
+import express from "express";
+import { initDB } from "./config/db.js";
 
-import express from 'express';
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
