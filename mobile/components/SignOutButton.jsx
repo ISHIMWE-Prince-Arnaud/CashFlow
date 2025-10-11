0864065a21d7927bceb53a0d2fa867bd7ab7a50a
ISH k pro <princeishimwe754@gmail.com>
2025-10-11 12:35:11 +0200

    Changed text sign out to an Icon

--- mobile/components/SignOutButton.jsx
+++ mobile/components/SignOutButton.jsx
@@ -1,24 +1,24 @@
-import { useClerk } from '@clerk/clerk-expo'
-import * as Linking from 'expo-linking'
-import { Text, TouchableOpacity } from 'react-native'
+import React from 'react';
+import { useClerk } from '@clerk/clerk-expo';
+import * as Linking from 'expo-linking';
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export const SignOutButton = () => {
-  // Use `useClerk()` to access the `signOut()` function
-  const { signOut } = useClerk()
+  const { signOut } = useClerk();
+
   const handleSignOut = async () => {
     try {
-      await signOut()
-      // Redirect to your desired page
-      Linking.openURL(Linking.createURL('/'))
+      await signOut();
+      Linking.openURL(Linking.createURL('/'));
     } catch (err) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error(JSON.stringify(err, null, 2))
+      console.error(JSON.stringify(err, null, 2));
     }
-  }
+  };
+
   return (
-    <TouchableOpacity onPress={handleSignOut}>
-      <Text>Sign out</Text>
+    <TouchableOpacity onPress={handleSignOut} style={{ padding: 8 }}>
+      <Ionicons name="log-out-outline" size={24} color="#333" />
     </TouchableOpacity>
-  )
-}
+  );
+};
